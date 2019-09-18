c1fb4816bf166a121d20566ea5bf313bec86b6e6
isaac <Isaacboon@gmail.com>
2019-09-18 12:23:07 +1200

    mixed lights

---
+++ Assets/Enviroment/Scripts/RealtimeLightProximityController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealtimeLightProximityController : MonoBehaviour
+{
+
+    [SerializeField] float maxDistance = 10;
+    [SerializeField] GameObject thingToDisable;
+    [SerializeField] GameObject debugText;
+    [SerializeField] bool showDebugText = true;
+
+    private void Start()
+    {
+        debugText.SetActive(showDebugText);
+    }
+
+    void Update()
+    {
+        float distance = (PlayerManager.player.transform.position - transform.position).magnitude;
+        if (distance > maxDistance)
+        {
+            thingToDisable.SetActive(false);
+        }
+        else {
+            thingToDisable.SetActive(true);
+        }
+        
+    }
+}
