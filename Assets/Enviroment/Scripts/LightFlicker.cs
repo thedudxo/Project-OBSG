fa3f3bd4d530ac09e5c6b3155a7cf9a9da816566
isaac <Isaacboon@gmail.com>
2019-09-19 15:27:22 +1200

    i suppose

--- Assets/Enviroment/Scripts/LightFlicker.cs
+++ Assets/Enviroment/Scripts/LightFlicker.cs
@@ -6,25 +6,57 @@ public class LightFlicker : MonoBehaviour
 {
 
     Light light;
-    [SerializeField] float min;
-    [SerializeField] float max;
+    [SerializeField] float minIntensity;
+    [SerializeField] float maxIntensity;
+
+    [SerializeField] float minRange;
+    [SerializeField] float maxRange;
+
+    [SerializeField] float heightBobble;
+    float startHeight;
 
     // Start is called before the first frame update
     void Start()
     {
         light = GetComponent<Light>();
+        startHeight = light.gameObject.transform.position.y;
     }
 
     void Update()
     {
-
-        Mathf.Clamp(light.intensity, min, max);
+        //intensity
+        Mathf.Clamp(light.intensity, minIntensity, maxIntensity);
 
         if (Random.Range(0, 2) == 1)
             light.intensity += 0.05f;
         else
             light.intensity += -0.05f;
 
+
+        //range
+        Mathf.Clamp(light.range, minRange, maxRange);
+
+        if (Random.Range(0, 2) == 1)
+            light.range += 0.05f;
+        else
+            light.range += -0.05f;
+
+
+        //height
         
+        Vector3 pos = light.gameObject.transform.position;
+
+        if (Random.Range(0, 2) == 1)
+            pos.y += 0.001f;
+        else
+            pos.y += -0.001f;
+
+        Mathf.Clamp(pos.y, startHeight + heightBobble, startHeight - heightBobble);
+
+        light.gameObject.transform.position = pos;
     }
+
 }
+
+
+
