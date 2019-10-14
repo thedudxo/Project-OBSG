3a9afef48d2a675dd06875546c818924694667be
isaac <Isaacboon@gmail.com>
2019-10-14 13:13:59 +1300

    Dusting out the door

---
+++ Assets/Enviroment/Scripts/Unlockable Door/Jank/Keyhole.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyhole : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void unlock() {
+        animator.Play("Key Animation");
+    }
+}
---
+++ Assets/Enviroment/Scripts/Unlockable Door/Jank/Locks.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locks : MonoBehaviour
+{
+
+    [SerializeField] GameObject helpText;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == Tags.PLAYER)
+        {
+            helpText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == Tags.PLAYER)
+        {
+            helpText.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log("workkkkking");
+        if (helpText.activeSelf)
+        {
+            Debug.Log("working");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+            }
+        }
+    }
+}
