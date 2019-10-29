6b36df782362ed44485641b0f43f24a98fc5651d
Jordan <jordanclark96@hotmail.com>
2019-10-29 15:20:24 +1300

    MoreNothing

--- Assets/Player/Scripts/Camera/MouseLook.cs
+++ Assets/Player/Scripts/Camera/MouseLook.cs
@@ -22,12 +22,15 @@ public class MouseLook : MonoBehaviour {
         currentMouseLook, 
         smoothMove;
 
-    private float currentRollAngle;
+    private float 
+        currentRollAngle,
+        xAxisClamp;
 
     private int lastLookFrame;
 
 	void Start () {
         Cursor.lockState = CursorLockMode.Locked;
+        xAxisClamp = 0;
         Application.targetFrameRate = 60;
 	}
 	
@@ -51,27 +54,51 @@ public class MouseLook : MonoBehaviour {
     }
 
     void LookAround() {
-        float mouseX = Input.GetAxis(MouseAxis.MOUSE_X);
-        float mouseY = Input.GetAxis(MouseAxis.MOUSE_Y);
+        Debug.Log(PlayerManager.invert);
+        lookAngles.x = Input.GetAxis(MouseAxis.MOUSE_Y) * PlayerManager.sensitivity * 50 * Time.deltaTime * (PlayerManager.invert ? -1f : 1f);
+        lookAngles.y = Input.GetAxis(MouseAxis.MOUSE_X) * PlayerManager.sensitivity * 50 * Time.deltaTime;
 
-        float rotAmountX = mouseX * PlayerManager.sensitivity;
-        float rotAmountY = mouseY * PlayerManager.sensitivity * (PlayerManager.invert ? -1f : 1f);
+        xAxisClamp += lookAngles.x;
 
-        lookAngles.x = Mathf.Clamp(lookAngles.x, defaultLookLimits.x, defaultLookLimits.y);
+        if(xAxisClamp > 80) {
+            xAxisClamp = 80;
+            lookAngles.x = 0;
+            ClampXAxis(280);
+        } else if (xAxisClamp < -70) {
+            xAxisClamp = -70;
+            lookAngles.x = 0;
+            ClampXAxis(70);
+        }
+        transform.Rotate(Vector3.left * lookAngles.x);
+        playerRoot.Rotate(Vector3.up * lookAngles.y);
+    }
 
-        Vector3 targetRotCam = transform.rotation.eulerAngles;
-        Vector3 targetRotBody = playerRoot.rotation.eulerAngles;
-
-        currentRollAngle = Mathf.Lerp(currentRollAngle, Input.GetAxisRaw(MouseAxis.MOUSE_X) * rollAngle, Time.deltaTime * rollSpeed);
-
-        targetRotCam.x -= rotAmountY;
-        targetRotCam.z = currentRollAngle;
-        targetRotBody.y += rotAmountX;
-
-        transform.rotation = Quaternion.Euler(targetRotCam);
-        playerRoot.rotation = Quaternion.Euler(targetRotBody);
+    void ClampXAxis(float value) {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
     }
 }
+//        float mouseX = Input.GetAxis(MouseAxis.MOUSE_X);
+//        float mouseY = Input.GetAxis(MouseAxis.MOUSE_Y);
+//
+//        lookAngles.y = mouseX * PlayerManager.sensitivity;
+//        lookAngles.x = mouseY * PlayerManager.sensitivity * (PlayerManager.invert ? -1f : 1f);
+//
+//        lookAngles.x = Mathf.Clamp(lookAngles.x, defaultLookLimits.x, defaultLookLimits.y);
+//
+//        Vector3 targetRotCam = transform.rotation.eulerAngles;
+//        Vector3 targetRotBody = playerRoot.rotation.eulerAngles;
+//
+//        currentRollAngle = Mathf.Lerp(currentRollAngle, Input.GetAxisRaw(MouseAxis.MOUSE_X) * rollAngle, Time.deltaTime * rollSpeed);
+//
+//        targetRotCam.x -= lookAngles.x;
+//        targetRotCam.z = currentRollAngle;
+//        targetRotBody.y += lookAngles.y;
+//
+//        transform.rotation = Quaternion.Euler(targetRotCam);
+//        playerRoot.rotation = Quaternion.Euler(targetRotBody);
+
 /*
         currentMouseLook = new Vector2(Input.GetAxis(MouseAxis.MOUSE_Y), Input.GetAxis(MouseAxis.MOUSE_X));
         lookAngles.x += currentMouseLook.x * PlayerManager.sensitivity * (PlayerManager.invert ? 1f : -1f);
