757d0e8a9306fd6d0da4e03fd20e3605e67432e4
isaac <Isaacboon@gmail.com>
2019-11-13 13:16:42 +1300

    quicksaving

--- Assets/_Managers/LoadManager.cs
+++ Assets/_Managers/LoadManager.cs
@@ -5,17 +5,18 @@ using UnityEngine.SceneManagement;
 
 public  static class LoadManager
 {
-    public static int sceneToLoad = 2;
-    public static int LoadedScene = 0;
-
     public const int MAINMENU = 0;
     public const int LOADING = 1;
     public const int TUTORIAL = 2;
     public const int LEVEL1 = 3;
 
+    public static int sceneToLoad = TUTORIAL;
+    public static int LoadedScene = 0;
+
     public static void loadScene(int scene)
     {
         sceneToLoad = scene;
+        LoadedScene = scene;
         SceneManager.LoadScene(LOADING);
     }
 }
