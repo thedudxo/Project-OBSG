04a3107e2619c833603b9146f5b3b6a91480d02a
isaac <Isaacboon@gmail.com>
2019-11-15 12:12:04 +1300

    Main Menuing

---
+++ Assets/Enviroment/Scripts/Locked Sections/UnlockDoorWhenEnimiesKilled.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockDoorWhenEnimiesKilled : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] enemiesToKill;
+    [SerializeField] GameObject thingToDisable;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int deadEnemies = 0;
+        foreach (GameObject enemy in enemiesToKill){
+            if (enemy.GetComponent<EnemyDeathScript>().dead)
+            {
+                deadEnemies++;
+            }
+        }
+
+        if(deadEnemies >= enemiesToKill.Length)
+        {
+            thingToDisable.SetActive(false);
+        }
+    }
+}
