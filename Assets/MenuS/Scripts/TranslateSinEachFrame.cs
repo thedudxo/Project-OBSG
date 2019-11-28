939b2305c5ae98f44febf028aa3dc3723b5d34fd
isaac <Isaacboon@gmail.com>
2019-11-28 13:15:37 +1300

    fancyish menu

---
+++ Assets/MenuS/Scripts/TranslateSinEachFrame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateSinEachFrame : MonoBehaviour
+{
+    [SerializeField] Vector3 speed = new Vector3(0, 0, 0);
+    [SerializeField] Vector3 intensity = new Vector3(0, 0, 0);
+    Vector3 position;
+    float sin = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sin += speed.x;
+
+        transform.position = new Vector3(
+            position.x + Mathf.Sin(sin) * intensity.x,
+            position.y, position.z);
+    }
+}
