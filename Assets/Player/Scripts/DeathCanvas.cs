63d93e6557f1232f8d995f5b02ed8c0f4df69a53
Jordan <jordanclark96@hotmail.com>
2019-12-06 13:05:54 +1300

    deathScreen

---
+++ Assets/Player/Scripts/DeathCanvas.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathCanvas : MonoBehaviour
+{
+
+    [SerializeField] Animator dead;
+
+    public void DeathScreen() {
+        dead.SetTrigger("Dead");
+    }
+}
